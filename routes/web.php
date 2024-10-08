fce7e2448317c98c39800ba4f9740f6f77c44635
Carlos Miranda <carlosperez280202@gmail.com>
2024-10-07 19:41:48 -0600

    requerimiento de login

--- routes/web.php
+++ routes/web.php
@@ -28,25 +28,26 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/crearorden', [crearOrdenController::class, 'index'])->name('ordenes.crearorden');
-Route::get('/listadoOrden', [listadoOrdenController::class, 'index'])->name('ordenes.index');
-Route::get('/clientes', [listadoClienteController::class, 'index'])->name('clientes.index');
-Route::put('/cliente/update', [listadoClienteController::class, 'update'])->name('cliente.update');
-Route::post('/clientes/store', [listadoClienteController::class, 'store'])->name('cliente.store');
-Route::post('/ordenes/store', [crearOrdenController::class, 'store'])->name('ordenes.store');
-Route::get('/ordenes/{id}', [listadoOrdenController::class, 'show'])->name('ordenes.show');
-Route::get('/deudores', [DeudoresController::class, 'index'])->name('deudores.index');
-Route::get('/deudores/documentos/{idCliente}', [DeudoresController::class, 'getDocumentosDeudores'])->name('deudores.documentos');
-Route::get('/deudores/anticipos/{idCliente}', [DeudoresController::class,'getAnticipos'])->name('deudores.anticipos');
-Route::get('/deudores/pagos-aplicados/{idDocumento}', [DeudoresController::class, 'getPagosAplicados'])->name('deudores.pagos_aplicados');
-Route::post('/pagos/store', [DeudoresController::class, 'store'])->name('pagos.store');
-Route::get('/consulta_pagos', [Consulta_PagosController::class, 'index'])->name('deudores.consulta_pagos');
-Route::get('/listado_deudores', [ListadoDeudoresController::class, 'index'])->name('deudores.listado_deudores');
-Route::get('/impresion_orden/{id}', [ListadoOrdenController::class, 'generateReport'])->name('voting-result');
-Route::get('/impresion_pago/{id}', [Consulta_PagosController::class, 'generateReport'])->name('pago_impresion');
-Route::get('/listado_movimientos', [ListadoMovimientosController::class, 'index'])->name('movimientos.index');
-Route::get('/bancos', [ListadoBancosController::class, 'index'])->name('movimientos.bancos');
-Route::get('/cuentas_bancarias', [ListadoCuentasController::class, 'index'])->name('movimientos.cuentas');
-Route::post('/movimiento/store', [ListadoMovimientosController::class, 'store'])->name('movimiento.store');
-Route::post('/anticipos', [DeudoresController::class, 'storeAnticipo'])->name('anticipos.store');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/crearorden', [crearOrdenController::class, 'index'])->name('ordenes.crearorden');
+    Route::get('/listadoOrden', [listadoOrdenController::class, 'index'])->name('ordenes.index');
+    Route::get('/clientes', [listadoClienteController::class, 'index'])->name('clientes.index');
+    Route::put('/cliente/update', [listadoClienteController::class, 'update'])->name('cliente.update');
+    Route::post('/clientes/store', [listadoClienteController::class, 'store'])->name('cliente.store');
+    Route::post('/ordenes/store', [crearOrdenController::class, 'store'])->name('ordenes.store');
+    Route::get('/ordenes/{id}', [listadoOrdenController::class, 'show'])->name('ordenes.show');
+    Route::get('/deudores', [DeudoresController::class, 'index'])->name('deudores.index');
+    Route::get('/deudores/documentos/{idCliente}', [DeudoresController::class, 'getDocumentosDeudores'])->name('deudores.documentos');
+    Route::get('/deudores/anticipos/{idCliente}', [DeudoresController::class,'getAnticipos'])->name('deudores.anticipos');
+    Route::get('/deudores/pagos-aplicados/{idDocumento}', [DeudoresController::class, 'getPagosAplicados'])->name('deudores.pagos_aplicados');
+    Route::post('/pagos/store', [DeudoresController::class, 'store'])->name('pagos.store');
+    Route::get('/consulta_pagos', [Consulta_PagosController::class, 'index'])->name('deudores.consulta_pagos');
+    Route::get('/listado_deudores', [ListadoDeudoresController::class, 'index'])->name('deudores.listado_deudores');
+    Route::get('/impresion_orden/{id}', [ListadoOrdenController::class, 'generateReport'])->name('voting-result');
+    Route::get('/impresion_pago/{id}', [Consulta_PagosController::class, 'generateReport'])->name('pago_impresion');
+    Route::get('/listado_movimientos', [ListadoMovimientosController::class, 'index'])->name('movimientos.index');
+    Route::get('/bancos', [ListadoBancosController::class, 'index'])->name('movimientos.bancos');
+    Route::get('/cuentas_bancarias', [ListadoCuentasController::class, 'index'])->name('movimientos.cuentas');
+    Route::post('/movimiento/store', [ListadoMovimientosController::class, 'store'])->name('movimiento.store');
+    Route::post('/anticipos', [DeudoresController::class, 'storeAnticipo'])->name('anticipos.store');
+});
