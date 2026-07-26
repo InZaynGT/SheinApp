0422a3e57da5162376062ee97badf8f1551c78de
InZaynGT <carlosperez280202@gmail.com>
2026-07-26 13:31:37 -0600

    cambio

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://tofenterprise.org'),
+    'url' => env('APP_URL', 'https://boutique.tofenterprise.org'),
 
     'asset_url' => env('ASSET_URL', null),
 
