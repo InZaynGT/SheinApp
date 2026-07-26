7b5a0f688668ffe4d9a0b98f75560a015dae2556
InZaynGT <carlosperez280202@gmail.com>
2026-07-26 13:38:12 -0600

    cambios welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -160,7 +160,7 @@
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="card category-card">
-                        <img src="../../public/vendor/adminlte/dist/img/accesorios.jpg" class="card-img-top" alt="Accesorios">
+                        <img src="{{ config('app.url') }}/vendor/adminlte/dist/img/accesorios.jpg" class="card-img-top" alt="Ropa">
                         <div class="card-body text-center">
                             <p class="category-title">Accesorios</p>
                             <a href="#" class="btn btn-gold">Ver más</a>
@@ -169,7 +169,7 @@
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="card category-card">
-                        <img src="../../public/vendor/adminlte/dist/img/zapatos.jpg" class="card-img-top" alt="Zapatos">
+                        <img src="{{ config('app.url') }}/vendor/adminlte/dist/img/zapatos.jpg" class="card-img-top" alt="Ropa">
                         <div class="card-body text-center">
                             <p class="category-title">Zapatos</p>
                             <a href="#" class="btn btn-gold">Ver más</a>
@@ -178,7 +178,7 @@
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4">
                     <div class="card category-card">
-                        <img src="../../public/vendor/adminlte/dist/img/images.png" class="card-img-top" alt="Hogar">
+                        <img src="{{ config('app.url') }}/vendor/adminlte/dist/img/images.jpg" class="card-img-top" alt="Ropa">
                         <div class="card-body text-center">
                             <p class="category-title">Hogar</p>
                             <a href="#" class="btn btn-gold">Ver más</a>
