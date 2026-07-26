563978c597928a185153c9a34a0c779f1669a899
InZaynGT <carlosperez280202@gmail.com>
2026-07-26 13:20:40 -0600

    bootstrap

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -83,8 +83,6 @@
     {{-- Base Scripts --}}
     @if(!config('adminlte.enabled_laravel_mix'))
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
