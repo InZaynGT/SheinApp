70abea762b7df09c97e8421fc059e3d2e4cdc12d
InZaynGT <carlosperez280202@gmail.com>
2026-07-26 13:03:39 -0600

    agregar bootstrap

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -171,6 +171,7 @@
 </div>
 
 @push('js')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         // Modal de edición
